bdf63d84059a93e1fabc680aa75758807b6fb864
faiyazsadi <nabil.faiyaz@dsinnovators.com>
2024-08-08 16:44:37 +0600

    alt: using only userDetailService

--- src/main/java/org/example/springsecurity/controller/WebController.java
+++ src/main/java/org/example/springsecurity/controller/WebController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
-    @GetMapping("/public")
+    @GetMapping("")
     public String hello() {
         return "views/public";
     }
